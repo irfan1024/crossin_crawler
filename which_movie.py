df09d72a9e851be4af6280a6f59dfea759c6f3a5
irfan1024 <815282067@qq.com>
2019-08-29 22:52:08 +0800

    finish

--- which_movie.py
+++ which_movie.py
@@ -20,24 +20,26 @@ def image():
 #批量获取电影数据并保存为 csv 文件函数
 def data_csv():
     rows = []
-    for i in range(0, 40, 20):
-        url = ' https://api.douban.com/v2/movie/top250?start=%d&apikey=0df993c66c0c636e29ecbb5344252a4a' % i
-        req = requests.get(url)
-        json_dict = req.json()
-        subjects = json_dict['subjects']
-        # 批量获取电影数据
-        for subject in subjects:
-            id = subject['id']
-            name = subject['title']
-            star = subject['rating']['average']
-            casts = subject['casts']
-            actor = ''
-            alt = subject['alt']
-            #循环获取演员
-            for cast in casts:
-                actor += cast['name'] + ', '
-            row = (id, name, star, actor, alt)
-            rows.append(row)
+    for i in range(0, 250, 20):
+        try:
+            url = ' https://api.douban.com/v2/movie/top250?start=%d&apikey=0df993c66c0c636e29ecbb5344252a4a' % i
+            req = requests.get(url)
+            json_dict = req.json()
+            subjects = json_dict['subjects']
+        except:
+            # 批量获取电影数据
+            for subject in subjects:
+                id = subject['id']
+                name = subject['title']
+                star = subject['rating']['average']
+                casts = subject['casts']
+                actor = ''
+                alt = subject['alt']
+                #循环获取演员
+                for cast in casts:
+                    actor += cast['name'] + ', '
+                row = (id, name, star, actor, alt)
+                rows.append(row)
         #写入 csv 文件
         header = ['id', '电影名称', '评分', '演员', '网址']
         with open('movie.csv', 'w', newline='') as f:
@@ -49,4 +51,5 @@ def data_csv():
 
 #主函数
 if __name__ == '__main__':
-    data_csv()
+    # data_csv()
+    image()
--- zhihu.py
+++ zhihu.py
@@ -11,14 +11,18 @@ def file_w(rows):
 
 #获取知乎时间线上的问题的函数
 def zhihu(url, headers):
-    req = requests.session().get(url, headers=headers)
-    dict_json = req.json()
-    datas = dict_json['data']
-    rows = []
-
+    try:
+        req = requests.session().get(url, headers=headers)
+        print('type', type(req))
+        dict_json = req.json()
+        datas = dict_json['data']
+        rows = []
+    except:
+        print('获取网站数据错误')
+        return 0
     for data in datas:
-        type = data['target']['type']
-        if type == 'article':
+        data_type = data['target']['type']
+        if data_type == 'article':
             header = ['id', '标题', '赞', '作者', '评论数']
             id = data['target']['id']
             title = data['target']['title']
@@ -27,7 +31,7 @@ def zhihu(url, headers):
             comment_count = data['target']['comment_count']
             row = (id, title, voteup_count, author, comment_count)
 
-        elif type == 'answer':
+        elif data_type == 'answer':
             id = data['target']['id']
             action_text = data['action_text']
             voteup_count = data['target']['voteup_count']
@@ -40,73 +44,25 @@ def zhihu(url, headers):
             row = (id, action_text, voteup_count, thanks_count, comment_count, question, author, answer_count, follower_count)
             header = ['id', '标签', '赞', '感谢', '反对', '问题', '提问者', '回答数', '关注数']
         else:
+            print("未找到数据")
             continue
         rows.append(header)
         rows.append(row)
-    file_w(rows)
+    try:
+        file_w(rows)
+    except:
+        print("写入文件错误")
 
 
+#主函数
 if __name__ == '__main__':
     #url, headers
     url = 'https://www.zhihu.com/api/v3/feed/topstory/recommend'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'
     cookies = '_zap=9d702648-1c02-4d51-8a5f-ba5be53c05d7; _xsrf=c0347064-31c6-4a06-9175-dc0acc647bb7; d_c0="AABhptIh8Q-PTtc6uV1qBbPWkPhI_E5coZk=|1566660140"; capsion_ticket="2|1:0|10:1566780547|14:capsion_ticket|44:ZWZkMjhmMDA4ZGJjNGE2OGE2ZDVkNWJmNjFhZWRjOTU=|1de7c7f5ce5fba7ad373bba5255017c3df389bbede623a1d8c46f9fbdb0604a3"; z_c0="2|1:0|10:1566780551|4:z_c0|92:Mi4xODVBVEFBQUFBQUFBQUdHbTBpSHhEeVlBQUFCZ0FsVk5obnBRWGdENU5WRW9WQ1pKVTVLOG1nU2YxRk9nX2VhaTZB|6a93da3335d2400f5806be5481658a427adf9f4f7e58c0812807da549dea4990"; q_c1=7cee8b00ac7f440eb8030707e630ea45|1566783010000|1566783010000; tshl=; tst=r; tgw_l7_route=73af20938a97f63d9b695ad561c4c10c'
-
     headers = {
         "User-Agent": user_agent,
         'Cookie': cookies,
     }
     zhihu(url, headers)
 
-#
-#
-# import os
-# import csv
-# import requests
-# from http import HTTPStatus
-#
-# class ZhihuRecommend:
-#     URL = "https://www.zhihu.com/api/v3/feed/topstory/recommend"
-#
-#     def __init__(self, url=URL):
-#         self.url = url
-#         self.web = requests.session()
-#
-#     def get_page_data(self):
-#         headers = {
-#                 'cookie': '_zap=43eaded6-a732-4ee7-85b4-c7ac8c2614c5; d_c0="ABDnkMYHMQ-PTui_mptmfL3t4WcTSx9KIug=|1553768410"; _xsrf=Zgy0cAoMKmR8LXorsOrwWH9688apvRiV; z_c0="2|1:0|10:1554286165|4:z_c0|92:Mi4xOGdxRUFnQUFBQUFBRU9lUXhnY3hEeVlBQUFCZ0FsVk5WZFNSWFFBMTBWX1F5Q1gyUHl5dF9KM0J2Z0Vhc01udzdR|3e8f2f8fa63645be91387e6b259b8dfdc6dd278f9c99a010b500b2c860a1e49b"; q_c1=77ff3c79cc414cd6980fc4df6367c896|1563535619000|1556448479000; tst=r; __utma=51854390.1825543909.1565075465.1565075465.1565075465.1; __utmz=51854390.1565075465.1.1.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/people/napoleon-lee/collections; __utmv=51854390.100-1|2=registration_date=20160124=1^3=entry_date=20160124=1; tgw_l7_route=66cb11',
-#                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
-#                 }
-#         params = {
-#                 'session_token': 'b3b8777f76c7d1d0e2717ddca35d9aae',
-#                 'desktop': 'true',
-#                 'page_number': 2,
-#                 'limit': 6,
-#                 'action': 'down',
-#                 'after_id': 5
-#                 }
-#         r = self.web.get("{}".format(self.url), headers=headers, params=params)
-#         return r.json()
-#
-#     def save_csv(self):
-#         rows = []
-#         headers = ["id","type","offset","verb","created_time","updated_time","target","brief","uninterest_reasons",
-#                 "attached_info","actors","show_actor_time","action_text","action_text_tpl","action_card"]
-#         data = self.get_page_data()
-#         for i in range(len(data['data'])):
-#             rows.append(data['data'][i])
-#         os.makedirs('./zhihu/', exist_ok=True)
-#         with open("./zhihu/zhihu.csv", "w") as f:
-#             f_csv = csv.DictWriter(f, headers)
-#             f_csv.writeheader()
-#             f_csv.writerows(rows)
-#
-#     def run(self):
-#         print ("开始获取知乎时间线内容...")
-#         self.save_csv()
-#         print ("获取完成。")
-#
-#
-# if __name__ == "__main__":
-#     zhihu = ZhihuRecommend()
-#     zhihu.run()
